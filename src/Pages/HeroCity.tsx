e0c154025f27c98ff1fcdc2e4a3b1d913feca084
Iranigami Sonorous <iranigami@mail.ru>
2025-05-15 23:25:00 +0400

    aaaaaaaaaa

--- src/Pages/HeroCity.tsx
+++ src/Pages/HeroCity.tsx
@@ -9,15 +9,7 @@ export default function HeroCity(){
     return(
         <>
         <div>
-            <iframe hidden={true} src="https://may9.ru/our-victory/city-hero/stalingrad" width="3840" height="2160"> </iframe>
-            <div className="w-full h-full fixed top-0 text-center text-[150px] bg-[#f5f5f5] text-orange-500 font-montserrat font-normal p-[200px]">
-                <span className="text-red-500 font-bold text-[160px]">Это тестовая страница!</span><br/>
-                Тут должна быть страница с городом {cityName}, но владельцы сайта may9.ru запретили его встраивание. Можете перейти по&nbsp;
-                <a href={"https://may9.ru/our-victory/city-hero/" + cityName} className="text-red-500 underline">
-                    ссылке
-                </a>
-                , для проверки её корректности, а я пока буду думать, как это решать...
-            </div>
+            <iframe hidden={false} src={"https://may9.ru/our-victory/city-hero/" + cityName} className="doc scale-[200%] fixed top-[25%] left-[25%]" width="1920" height="1080"></iframe>
             <button 
                 onClick={() => navigate("/home")}
                 className="w-[440px] h-[128px] rounded-[64px] bg-white flex gap-[16px] shadow-[0px_2px_20px_0px_#00000026] fixed bottom-[40px] left-[40px] justify-center items-center font-montserrat font-bold text-[40px] text-[#EE8545] leading-[100%] tracking-0">
